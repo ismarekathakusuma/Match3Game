d1f186efc102a8542bbc4aaaa4f292129dfe6f8f
Rekatha <kenal.rekatha@gmail.com>
2022-04-26 14:41:02 +0700

    Re-fill board so not runs out of tiles

--- Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
+++ Assets/Match 3 Starter/Scripts/Board and Grid/BoardManager.cs
@@ -87,7 +87,17 @@ public class BoardManager : MonoBehaviour {
 				}
 			}
 		}
+
+		for (int x = 0; x < xSize; x++)
+		{
+			for (int y = 0; y < ySize; y++)
+			{
+				tiles[x, y].GetComponent<Tile>().ClearAllMatches();
+			}
+		}
+
 	}
+
 
 	private IEnumerator ShiftTilesDown(int x, int yStart, float shiftDelay = .03f)
 	{
@@ -111,11 +121,31 @@ public class BoardManager : MonoBehaviour {
 			for (int k = 0; k < renders.Count - 1; k++)
 			{ // Loop through every SpriteRenderer in the list of renders
 				renders[k].sprite = renders[k + 1].sprite;
-				renders[k + 1].sprite = null; // Swap each sprite with the one above it, until the end is reached and the last sprite is set to null
+				renders[k + 1].sprite = GetNewSprite(x, ySize - 1);
 			}
 		}
 		IsShifting = false;
 	}
 
+	private Sprite GetNewSprite(int x, int y)
+	{
+		List<Sprite> possibleCharacters = new List<Sprite>();
+		possibleCharacters.AddRange(characters);
+
+		if (x > 0)
+		{
+			possibleCharacters.Remove(tiles[x - 1, y].GetComponent<SpriteRenderer>().sprite);
+		}
+		if (x < xSize - 1)
+		{
+			possibleCharacters.Remove(tiles[x + 1, y].GetComponent<SpriteRenderer>().sprite);
+		}
+		if (y > 0)
+		{
+			possibleCharacters.Remove(tiles[x, y - 1].GetComponent<SpriteRenderer>().sprite);
+		}
+
+		return possibleCharacters[Random.Range(0, possibleCharacters.Count)];
+	}
 
 }
